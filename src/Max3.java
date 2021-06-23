2f25321757328567f36bff89e875fd5909dc5a48
kaveloper <tsd03150@naver.com>
2021-06-24 02:36:44 +0900

    update

--- src/Max3.java
+++ src/Max3.java
@@ -1,6 +1,26 @@
+// 세 개의 정수를 입력 받아서 MAX값 찾는 프로그램
+
+
+import java.util.Scanner;
+
 public class Max3 {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("첫번째 값을 입력해주세요. >> "); int a = stdIn.nextInt();
+        System.out.print("두번째 값을 입력해주세요. >> "); int b = stdIn.nextInt();
+        System.out.print("세번째 값을 입력해주세요. >> "); int c = stdIn.nextInt();
+
+        int max = a;
+        if (b > max) {
+            max = b;
+        }
+
+        if (c > max) {
+            max = c;
+        }
+
+        System.out.println("최대값은 " + max +" 입니다.");
     }
 }
---
+++ src/Max3m.java
@@ -0,0 +1,86 @@
+
+
+public class Max3m {
+
+    // 세 개의 정수를 입력 받아서 MAX값 찾는 메서드
+    static int max3(int a, int b, int c) {
+        int max = a;
+
+        if (b > max) {
+            max = b;
+        }
+        if (c > max) {
+            max = c;
+        }
+
+        return max;
+    }
+
+    // 네 개의 정수를 입력 받아서 MAX값 찾는 메서드
+    static int max4(int a, int b, int c, int d) {
+        int max = a;
+
+        if (b > max) {
+            max = b;
+        }
+        if (c > max) {
+            max = c;
+        }
+        if (d > max) {
+            max = d;
+        }
+        return max;
+    }
+
+    // 세 개의 정수를 입력 받아서 MIN값 찾는 메서드
+    static int min3(int a, int b, int c) {
+        int min = a;
+
+        if (min > b) {
+            min = b;
+        }
+
+        if (min > c) {
+            min = c;
+        }
+        return min;
+    }
+
+    // 네 개의 정수를 입력 받아서 MAX값 찾는 메서드
+    static int min4(int a, int b, int c, int d) {
+        int min = a;
+
+        if (min > b) {
+            min = b;
+        }
+
+        if (min > c) {
+            min = c;
+        }
+
+        if (min > d) {
+            min = d;
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(min3(3,2,1));
+        System.out.println(min3(3,2,2));
+        System.out.println(min3(3,2,1));
+        System.out.println(min3(3,3,1));
+        System.out.println(min3(3,2,2));
+        System.out.println(min3(3,2,1));
+        System.out.println(min3(3,3,3));
+        System.out.println(min3(3,3,2));
+        System.out.println(min3(3,2,1));
+        System.out.println(min3(3,2,2));
+        System.out.println(min3(3,2,1));
+        System.out.println(min3(3,3,2));
+        System.out.println(min3(3,2,1));
+        System.out.println(min4(4,3,2,1));
+        System.out.println(min4(1,1,1,4));
+    }
+
+
+}
